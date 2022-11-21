e3267ae433b8b65afa985e5c93943c406cb6154a
MaxLeonel <32212027+MaxLeonel@users.noreply.github.com>
2022-11-21 17:41:58 -0300

    Actualizacion de Esena

--- src/Escena.jsx
+++ src/Escena.jsx
@@ -1,43 +1,39 @@
-import Phaser from "phaser";
-class Escena extends Phaser.Scene{
+import Phaser from 'phaser';
+
+class Escena extends Phaser.Scene {
     platforms = null;
     player = null;
     cursors = null;
-    star= null;
+    stars = null;
     score = 0;
-    scoreText;
+    scoreText = null;
     bombs = null;
+    gameOver = false;
 
 
-
-    preload (){
-
-    this.load.image('sky', 'img/sky.png');
-    this.load.image('ground','img/platform.png');
-    this.load.image('star', 'img/star.png');
-    this.load.image('bomb', 'img/bomb.png');
-    this.load.spritesheet('dude', 
-        'img/dude.png',
-        { frameWidth: 32, frameHeight: 48 });
-    
+    preload() {
+        this.load.image('sky', 'img/sky.png');
+        this.load.image('ground', 'img/platform.png');
+        this.load.image('star', 'img/star.png');
+        this.load.image('bomb', 'img/bomb.png');
+        this.load.spritesheet('dude', 'img/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
-    
 
-    create (){
+    create() {
+        
         this.add.image(400, 300, 'sky');
 
         this.platforms = this.physics.add.staticGroup();
-    
         this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    
         this.platforms.create(600, 400, 'ground');
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
 
-        this.bombs = this.physics.add.group();
-        
 
         this.player = this.physics.add.sprite(100, 450, 'dude');
+
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
@@ -50,7 +46,7 @@ class Escena extends Phaser.Scene{
 
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [{ key: 'dude', frame: 4 }],
             frameRate: 20
         });
 
@@ -61,76 +57,87 @@ class Escena extends Phaser.Scene{
             repeat: -1
         });
 
-        this.star = this.physics.add.group({
-            key: 'star',
-            repeat: 15,
-            setXY:{x:12, y:0,stepX:60}
-        });
-               
+        
+        this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        this.stars.children.iterate(function (child) {
+
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+        });
+
+        this.bombs = this.physics.add.group();
+
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.stars, this.platforms);
         this.physics.add.collider(this.bombs, this.platforms);
 
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+
         this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
-        
-        this.physics.add.collider(this.player,this.platforms);
-        this.physics.add.collider(this.star, this.platforms);
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.physics.add.overlap(this.player,this.star,this.collectStar,null,this);
-        this.scoreText= this.add.text(16,16, 'score: 0',{fontSize: '32px', fill: '#000'});
+
     }
 
-    update(){
-    if (this.cursors.left.isDown)
-{
-    this.player.setVelocityX(-160);
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
 
-    this.player.anims.play('left', true);
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
-    else if (this.cursors.right.isDown)
-    {
-        this.player.setVelocityX(160);
-
-        this.player.anims.play('right', true);
-    }
-    else
-    {
-        this.player.setVelocityX(0);
-
-        this.player.anims.play('turn');
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down)
-    {
-        this.player.setVelocityY(-330);
-    }
-    }
-
-    hitBomb (player, bomb){
-    this.physics.pause();
-
-    this.player.setTint(0xff0000);
-
-    this.player.anims.play('turn');
-}
-
-    collectStar (player, star){
+    collectStar(player, star) {
         star.disableBody(true, true);
 
         this.score += 10;
         this.scoreText.setText('Score: ' + this.score);
 
-        if (this.star.countActive(true) === 0){
-        this.stars.children.iterate(function (child) {
+        if (this.stars.countActive(true) === 0) {
+            this.stars.children.iterate(function (child) {
 
-            child.enableBody(true, child.x, 0, true, true);
+                child.enableBody(true, child.x, 0, true, true);
 
-        });
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-        var bomb = this.bombs.create(x, 16, 'bomb');
-        bomb.setBounce(0.2);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        }        
+            });
+
+            const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+            const bomb = this.bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            bomb.allowGravity = false;
+
+        }
+    }
+    hitBomb(player, bomb) {
+        this.physics.pause();
+
+        player.setTint(0xff0000);
+
+        player.anims.play('turn');
+
+        this.gameOver = true;
     }
 }
 export default Escena;
